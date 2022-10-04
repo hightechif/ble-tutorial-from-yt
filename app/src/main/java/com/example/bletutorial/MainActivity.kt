cfd63c5d02dd698147ecf282f6fe6af5355b7ec9
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2022-10-04 10:31:26 +0700

    SETUP: DI, bluetooth adapter and permission

--- app/src/main/java/com/example/bletutorial/MainActivity.kt
+++ app/src/main/java/com/example/bletutorial/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.bletutorial
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.bletutorial.presentation.Navigation
 import com.example.bletutorial.ui.theme.BLETutorialTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var bluetoothAdapter: BluetoothAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -15,4 +25,24 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        showBluetoothDialog()
+    }
+
+    private fun showBluetoothDialog() {
+        if(!bluetoothAdapter.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startBluetoothIntentForResult.launch(enableBluetoothIntent)
+        }
+    }
+
+    private val startBluetoothIntentForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode != Activity.RESULT_OK) {
+                showBluetoothDialog()
+            }
+        }
+
 }
