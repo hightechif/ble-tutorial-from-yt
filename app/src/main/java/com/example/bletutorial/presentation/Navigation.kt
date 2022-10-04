9d09a15e954371dc96f748da7543ea32eb929770
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2022-10-04 09:57:16 +0700

    FEAT: First UI Setup

---
+++ app/src/main/java/com/example/bletutorial/presentation/Navigation.kt
@@ -0,0 +1,28 @@
+package com.example.bletutorial.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.StartScreen.route) {
+        composable(Screen.StartScreen.route) {
+            StartScreen(navController = navController)
+        }
+
+        composable(Screen.TemperatureHumidityScreen.route) {
+            TemperatureHumidityScreen()
+        }
+    }
+
+}
+
+sealed class Screen(val route: String) {
+    object StartScreen: Screen("start_screen")
+    object TemperatureHumidityScreen: Screen("temp_humid_screen")
+}
